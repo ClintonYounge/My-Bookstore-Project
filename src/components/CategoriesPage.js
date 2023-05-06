b67184048a61de16b0ed6af1e3d6fab9330e2c73
ClintonYounge <clintonyounge18@gmail.com>
2023-05-06 13:36:48 -0400

    update navigation

--- src/components/CategoriesPage.js
+++ src/components/CategoriesPage.js
@@ -1,11 +1,8 @@
 // src/components/CategoriesPage.js
 
-import Header from './Header';
-
 export default function CategoriesPage() {
   return (
     <>
-      <Header />
       <h2>In the works</h2>
     </>
   );
