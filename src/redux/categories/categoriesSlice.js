d43387f5927f348b1f2eca325fa59837519e3f62
ClintonYounge <clintonyounge18@gmail.com>
2023-06-13 17:22:14 -0400

    update categoriesSlice

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -4,12 +4,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const categoriesSlice = createSlice({
   name: 'categories',
-  initialState: { categories: ['Under construction'] },
+  initialState: { categories: [] },
   reducers: {
-    checkStatus: (state) => ({
+    checkStatus: (state, action) => ({
       ...state,
-      categories: ['Under construction'],
+      categories: action.payload === 'Under construction' ? ['Under construction'] : state.categories,
     }),
+
   },
 });
 
